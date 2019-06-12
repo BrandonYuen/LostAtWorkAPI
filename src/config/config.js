809a781603e51fdbdcee62da25adfab7d40eacd4
BrandonYuen <brandon97@hotmail.com>
2019-06-12 21:33:27 +0200

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -2,5 +2,5 @@ const credentials = require('./credentials')
 
 module.exports = {
   jwtSecret: 'lost-at-work-pwa-brandonyuen',
-  db: 'mongodb+srv://'+credentials.dbUsername+':'+credentials.dbUsername+'@cluster0-bsq9g.mongodb.net/lostatwork'
+  db: 'mongodb+srv://'+credentials.dbUsername+':'+credentials.dbPassword+'@cluster0-bsq9g.mongodb.net/lostatwork'
 }
